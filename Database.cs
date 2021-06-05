f284279ff4c5425bdfc9a461551271c3e3153b1b
jojo9910 <jyotiyadav943349@gmail.com>
2021-06-05 15:34:41 +0530

    upd

---
+++ Database.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+namespace PerspectifyTut
+{
+    class Database
+    {
+        public SQLiteConnection myConnection;
+
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data Source=Bankdb.sqlite3");
+            if (!File.Exists("./Bankdb.sqlite3"))
+            {
+                SQLiteConnection.CreateFile("Bankdb.sqlite3");
+                Console.WriteLine("file has been created!");
+            }
+            else
+            {
+                Console.WriteLine("file is already there!");
+            }
+
+        }
+        public void OpenConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+        }
+        public void CloseConnetion()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Closed)
+            {
+                myConnection.Close();
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace PerspectifyTut
 {
@@ -39,13 +40,13 @@ namespace PerspectifyTut
 
         public void Display()
         {
-            Console.Write(HolderName+"  Your current account balance is: ");
+            Console.Write(HolderName + "  Your current account balance is: ");
             Console.Write(Balance);
         }
         public void Withdraw()
         {
             Console.WriteLine("Enter Amount to withdraw: ");
-            int Amount=Convert.ToInt32(Console.ReadLine());
+            int Amount = Convert.ToInt32(Console.ReadLine());
             if (this.CurrBalance - Amount >= 0)
             {
                 this.CurrBalance = this.CurrBalance - Amount;
@@ -72,37 +73,219 @@ namespace PerspectifyTut
             }
 
         }
-  
+
     }
     class Program
     {
 
+        public void Deposite(Database Dataobject)
+        {
+            try
+            {
+                Console.WriteLine("Enter Account HolderName: ");
+                string Name = Console.ReadLine();
+                Console.WriteLine("Enter Account number: ");
+                long AccountNo = Convert.ToInt64(Console.ReadLine());
+
+                Console.WriteLine("Enter Amount to Deposite: ");
+                int Amount = Convert.ToInt32(Console.ReadLine());
+                if (Amount > 0)
+                {
+                    string query = "UPDATE Users SET Balance =Balance+@amount WHERE AccountNo = @AccountNo";
+                    SQLiteCommand myCommand = new SQLiteCommand(query, Dataobject.myConnection);
+                    myCommand.Parameters.AddWithValue("@AccountNo", AccountNo);
+                    myCommand.Parameters.AddWithValue("@amount", Amount);
+                    if (myCommand.ExecuteNonQuery() == 1)
+                    {
+                        string q1 = "SELECT * FROM Users WHERE AccountNo=@AccountNo";
+                        SQLiteCommand find = new SQLiteCommand(q1, Dataobject.myConnection);
+                        find.Parameters.AddWithValue("@AccountNo", AccountNo);
+                        SQLiteDataReader result = find.ExecuteReader();
+                        while (result.Read())
+                        {
+                            Console.WriteLine("User: {0} - AccountNo.:{1} - CurrentBalanceis:{2}", result["HolderName"], result["AccountNO"], result["Balance"]);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Some error!!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Enter A Valid Amount!!");
+                }
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+
+        }
+
+        public void Display(Database Dataobject)
+        {
+            try
+            {
+                Console.WriteLine("Enter Account HolderName: ");
+                string Name = Console.ReadLine();
+                Console.WriteLine("Enter Account number: ");
+                long AccountNo = Convert.ToInt64(Console.ReadLine());
+                string query = "SELECT * FROM Users WHERE AccountNo=@AccountNo";
+                SQLiteCommand myCommand = new SQLiteCommand(query, Dataobject.myConnection);
+                myCommand.Parameters.AddWithValue("@AccountNo", AccountNo);
+                SQLiteDataReader result = myCommand.ExecuteReader();
+                if (result.HasRows)
+                {
+                    while (result.Read())
+                        Console.WriteLine("User: {0} - AccountNo.:{1} - CurrentBalanceis:{2}", result["HolderName"], result["AccountNo"], result["Balance"]);
+                }
+                else
+                {
+                    Console.WriteLine("Credentials wrong or user does not exits!");
+                    return;
+                }
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine("Error!!");
+                Console.WriteLine(err.Message);
+            }
+            
+        }
+
+        public void CreateNew(Database Dataobject) 
+        {
+            try
+            {
+                Console.WriteLine("Enter Account HolderName: ");
+                string HolderName = Console.ReadLine();
+                Console.WriteLine("Enter Account number: ");
+                long AccountNo = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine("Enter Account Type: ");
+                string Type = Console.ReadLine();
+                Console.WriteLine("Enter initial Amount: ");
+                int Amount = Convert.ToInt32(Console.ReadLine());
+                string Query = "INSERT INTO Users(`HolderName`,`AccountNo`,`Type`,`Balance`) VALUES(@HolderName,@AccountNo,@type,@Balance)";
+                SQLiteCommand InsertCommand = new SQLiteCommand(Query, Dataobject.myConnection);
+                InsertCommand.Parameters.AddWithValue("@HolderName", HolderName);
+                InsertCommand.Parameters.AddWithValue("@AccountNo", AccountNo);
+                InsertCommand.Parameters.AddWithValue("@type", Type);
+                InsertCommand.Parameters.AddWithValue("@Balance", Amount);
+                int res = InsertCommand.ExecuteNonQuery();
+                Console.WriteLine(res);
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+
+        public void Withdraw(Database Dataobject)
+        {
+            try
+            {
+                int CurrBalance = 0;
+                Console.WriteLine("Enter Account number: ");
+                long AccountNo = Convert.ToInt64(Console.ReadLine());
+                string query1 = "SELECT * FROM Users WHERE AccountNo=@AccountNo";
+                SQLiteCommand myCommand1 = new SQLiteCommand(query1, Dataobject.myConnection);
+                myCommand1.Parameters.AddWithValue("@AccountNo", AccountNo);
+                SQLiteDataReader result1 = myCommand1.ExecuteReader();
+                if (result1.HasRows)
+                {
+                    while (result1.Read())
+                       CurrBalance= Convert.ToInt32(result1["Balance"]);
+                }
+                else
+                {
+                    Console.WriteLine("Credentials wrong or user does not exits!");
+                    return;
+                }
+
+                Console.WriteLine("Enter Amount to Withdraw!: ");
+                int Amount = Convert.ToInt32(Console.ReadLine());
+                if (CurrBalance >= Amount)
+                {
+                    string query = "UPDATE Users SET Balance = CASE Balance" +
+                    "WHEN Balance>=@amount THEN Balance-@amount" +
+                    "ELSE Balance" +
+                    "END" +
+                    "WHERE AccountNo = @AccountNo";
+                    SQLiteCommand myCommand = new SQLiteCommand(query, Dataobject.myConnection);
+                    myCommand.Parameters.AddWithValue("@AccountNo", AccountNo);
+                    myCommand.Parameters.AddWithValue("@amount", Amount);
+                    if (myCommand.ExecuteNonQuery() == 1)
+                        Console.WriteLine("Done!");
+                    else
+                        Console.WriteLine("Query not executed!!");
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid amount!!");
+                    return;
+                }
+                
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+
+        }
+        public void AllRowsInTable(Database Dataobject)
+        {
+            try { 
+            string query = "SELECT * FROM Users";
+            SQLiteCommand myCommand1 = new SQLiteCommand(query, Dataobject.myConnection);
+            SQLiteDataReader result = myCommand1.ExecuteReader();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Console.WriteLine("User: {0} - AccountNo.:{1} - CurrentBalanceis:{2}", result["HolderName"], result["AccountNO"], result["Balance"]);
+                }
+            }
+        }
+            catch(Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+
+}
         static void Main(string[] args)
         {
+            Program p1 = new Program();
+            Database Dataobject = new Database();
+            Dataobject.OpenConnection();
+            p1.AllRowsInTable(Dataobject);
 
-            Bank b1 = new Bank("name",23323423,"savings");
             char agn;
-
-            do{
-
-                Console.WriteLine("Please Select Any Function!");
+            do
+            {
+               Console.WriteLine("Please Select Any Function!");
                 Console.WriteLine("\nPress 1 for Deposit an Amount. \nPress 2 for Withdraw an Amount. " +
-                    "\nPress 3 for Display account information.") ;
+                    "\nPress 3 for Display account information.\nPress 4 for Creating newAccount.");
                 int num = Convert.ToInt32(Console.ReadLine());
-                switch (num){
-                case 1:
-                  b1.Deposite();
-                  break;
+                
+                switch (num)
+                {
+                    case 1:
+                        p1.Deposite(Dataobject);
+                        break;
 
-                 case 2:
-                        b1.Withdraw();
+                    case 2:
+                        p1.Withdraw(Dataobject);
                         break;
-                 case 3:
-                        b1.Display();
+                    case 3:
+                        p1.Display(Dataobject);
                         break;
-                default:
-                 Console.WriteLine("Invalid Selection!!!");
-                 break;
+                    case 4:
+                        p1.CreateNew(Dataobject);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Selection!!!");
+                        break;
 
                 }
 
@@ -111,9 +294,13 @@ namespace PerspectifyTut
 
             } while (agn == 'y');
 
+            p1.AllRowsInTable(Dataobject);
+            Dataobject.CloseConnetion();
             Console.ReadLine();
 
         }
     }
+
+
 
 }
